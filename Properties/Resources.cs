b050629ef280f53c96ad413cd665aaf2f67ecc77
KrakenTW <ziolus03@gmail.com>
2021-06-03 01:23:17 +0200

    asd

---
+++ Properties/Resources.cs
@@ -0,0 +1,41 @@
+﻿
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace Rachunkowosc.Properties
+{
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [DebuggerNonUserCode]
+    [CompilerGenerated]
+    internal class Resources
+    {
+        private static ResourceManager resourceMan;
+        private static CultureInfo resourceCulture;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (Rachunkowosc.Properties.Resources.resourceMan == null)
+                    Rachunkowosc.Properties.Resources.resourceMan = new ResourceManager("Rachunkowosc.Properties.Resources", typeof(Rachunkowosc.Properties.Resources).Assembly);
+                return Rachunkowosc.Properties.Resources.resourceMan;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get => Rachunkowosc.Properties.Resources.resourceCulture;
+            set => Rachunkowosc.Properties.Resources.resourceCulture = value;
+        }
+    }
+}
---
+++ Szczegóły.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+
+public class Szczegóły : Window, IComponentConnector
+{
+            internal Button OkButton;
+            internal TextBox SzczegółyTB;
+            private bool _contentLoaded;
+
+            public Szczegóły() => this.InitializeComponent();
+
+            private void OkButton_Click(object sender, RoutedEventArgs e) => this.Close();
+
+            [DebuggerNonUserCode]
+            [GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
+            public void InitializeComponent()
+            {
+                if (this._contentLoaded)
+                    return;
+                this._contentLoaded = true;
+                Application.LoadComponent((object)this, new Uri("/Rachunkowość;component/szczeg%c3%b3%c5%82y.xaml", UriKind.Relative));
+            }
+
+            [DebuggerNonUserCode]
+            [GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
+            [EditorBrowsable(EditorBrowsableState.Never)]
+            void IComponentConnector.Connect(int connectionId, object target)
+            {
+                if (connectionId != 1)
+                {
+                    if (connectionId == 2)
+                        this.SzczegółyTB = (TextBox)target;
+                    else
+                        this._contentLoaded = true;
+                }
+                else
+                {
+                    this.OkButton = (Button)target;
+                    this.OkButton.Click += new RoutedEventHandler(this.OkButton_Click);
+                }
+            }
+        }
